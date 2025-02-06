e3032a5425b2e63aeaffa410db7b2d2b7e3d1b51
Dotan Tsur (local) <dotan.tsur@42-ds.com>
2025-02-06 15:52:29 +0200

    dummy mig process - partial

--- Implementation/SharedObjects/Java/src/com/k2view/cdbms/usercode/common/SharedGlobals.java
+++ Implementation/SharedObjects/Java/src/com/k2view/cdbms/usercode/common/SharedGlobals.java
@@ -11,6 +11,41 @@ public class SharedGlobals {
 	@desc("Migration Execution ID, set on start of migration execution")
 	public static String EXEC_ID = "-1";
 
+	public static String SF_LOAD_EXEC_STATUS = "N/A";
+
+	public static String SF_LOAD_COMPLETED = "0";
+
+	public static String SF_LOAD_REJECTED = "0";
+
+	public static String SF_LOAD_IN_PROC = "0";
+	public static String SF_LOAD_PENDING = "0";
+
+	public static String SF_CUST_EXEC_STATUS = "N/A";
+
+	public static String SF_CUST_COMPLET = "0";
+
+	public static String SF_CUST_REJECTED = "0";
+
+	public static String SF_CUST_IN_PROC = "0";
+
+	public static String SF_CUST_PENDING = "0";
+
+	public static String SF_ACNT_EXEC_STATUS = "N/.A";
+
+	public static String SF_ACNT_COMPLET = "0";
+	public static String SF_ACNT_REJECTED = "0";
+
+	public static String SF_ACNT_IN_PROC = "0";
+
+	public static String SF_ACNT_PENDING = "0";
+
+	public static String SF_ADRS_EXEC_STATUS = "N/A";
+
+	public static String SF_ADRS_COMPLET = "0";
+	public static String SF_ADRS_REJECTED = "0";
+	public static String SF_ADRS_IN_PROC = "0";
+	public static String SF_ADRS_PENDING = "0";
+
 	
 
 
